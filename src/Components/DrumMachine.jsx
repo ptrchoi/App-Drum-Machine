89e9cdcb5149c284de1b5aca75da9a053da06c40
Peter Choi <ptrchoi@gmail.com>
2020-03-25 16:32:54 -0500

    refactored audio files to be imported instead of referenced as links to update to current tech (wasn't working anymore)

--- src/Components/DrumMachine.jsx
+++ src/Components/DrumMachine.jsx
@@ -1,212 +1,227 @@
-import React from "react";
+// Libraries
+import React from 'react';
 
-//Components
-import DisplayBanks from "./DisplayBanks";
-import DisplayMessage from "./DisplayMessage";
-import DrumPad from "./DrumPad";
+// Components
+import DisplayBanks from './DisplayBanks';
+import DisplayMessage from './DisplayMessage';
+import DrumPad from './DrumPad';
+
+// Audio files
+import acoustic1 from '/audio/hihat-acoustic01.wav';
+import acoustic2 from '/audio/openhat-acoustic01.wav';
+import acoustic3 from '/audio/clap-808.wav';
+import acoustic4 from '/audio/snare-acoustic01.wav';
+import acoustic5 from '/audio/tom-acoustic01.wav';
+import acoustic6 from '/audio/perc-808.wav';
+import acoustic7 from '/audio/kick-big.wav';
+import acoustic8 from '/audio/kick-vinyl02.wav';
+import acoustic9 from '/audio/perc-tribal.wav';
+import funk1 from '/audio/hihat-electro.wav';
+import funk2 from '/audio/openhat-slick.wav';
+import funk3 from '/audio/perc-chirpy.wav';
+import funk4 from '/audio/snare-808.wav';
+import funk5 from '/audio/tom-808.wav';
+import funk6 from '/audio/snare-sumo.wav';
+import funk7 from '/audio/kick-tron.wav';
+import funk8 from '/audio/kick-zapper.wav';
+import funk9 from '/audio/perc-nasty.wav';
 
 //Sound Banks
 const ACOUSTIC_BANK = [
-  {
-    buttonId: "hi-hat",
-    audioId: "Q",
-    src: "./src/assets/audio/hihat-acoustic01.wav"
-  },
-  {
-    buttonId: "open-hat",
-    audioId: "W",
-    src: "./src/assets/audio/openhat-acoustic01.wav"
-  },
-  {
-    buttonId: "clap",
-    audioId: "E",
-    src: "./src/assets/audio/clap-808.wav"
-  },
-  {
-    buttonId: "snare",
-    audioId: "A",
-    src: "./src/assets/audio/snare-acoustic01.wav"
-  },
-  {
-    buttonId: "tom-tom",
-    audioId: "S",
-    src: "./src/assets/audio/tom-acoustic01.wav"
-  },
-  {
-    buttonId: "block",
-    audioId: "D",
-    src: "./src/assets/audio/perc-808.wav"
-  },
-  {
-    buttonId: "kick1",
-    audioId: "Z",
-    src: "./src/assets/audio/kick-big.wav"
-  },
-  {
-    buttonId: "kick2",
-    audioId: "X",
-    src: "./src/assets/audio/kick-vinyl02.wav"
-  },
-  {
-    buttonId: "wop",
-    audioId: "C",
-    src: "./src/assets/audio/perc-tribal.wav"
-  }
+	{
+		buttonId: 'hi-hat',
+		audioId: 'Q',
+		src: acoustic1
+	},
+	{
+		buttonId: 'open-hat',
+		audioId: 'W',
+		src: acoustic2
+	},
+	{
+		buttonId: 'clap',
+		audioId: 'E',
+		src: acoustic3
+	},
+	{
+		buttonId: 'snare',
+		audioId: 'A',
+		src: acoustic4
+	},
+	{
+		buttonId: 'tom-tom',
+		audioId: 'S',
+		src: acoustic5
+	},
+	{
+		buttonId: 'block',
+		audioId: 'D',
+		src: acoustic6
+	},
+	{
+		buttonId: 'kick1',
+		audioId: 'Z',
+		src: acoustic7
+	},
+	{
+		buttonId: 'kick2',
+		audioId: 'X',
+		src: acoustic8
+	},
+	{
+		buttonId: 'wop',
+		audioId: 'C',
+		src: acoustic9
+	}
 ];
 const FUNKY_BANK = [
-  {
-    buttonId: "electro-hat",
-    audioId: "Q",
-    src: "./src/assets/audio/hihat-electro.wav"
-  },
-  {
-    buttonId: "slick-hat",
-    audioId: "W",
-    src: "./src/assets/audio/openhat-slick.wav"
-  },
-  {
-    buttonId: "chirp",
-    audioId: "E",
-    src: "./src/assets/audio/perc-chirpy.wav"
-  },
-  {
-    buttonId: "80s-snare",
-    audioId: "A",
-    src: "./src/assets/audio/snare-808.wav"
-  },
-  {
-    buttonId: "80s-tom",
-    audioId: "S",
-    src: "./src/assets/audio/tom-808.wav"
-  },
-  {
-    buttonId: "sumo",
-    audioId: "D",
-    src: "./src/assets/audio/snare-sumo.wav"
-  },
-  {
-    buttonId: "tron-kick",
-    audioId: "Z",
-    src: "./src/assets/audio/kick-tron.wav"
-  },
-  {
-    buttonId: "zapper-kick",
-    audioId: "X",
-    src: "./src/assets/audio/kick-zapper.wav"
-  },
-  {
-    buttonId: "zip",
-    audioId: "C",
-    src: "./src/assets/audio/perc-nasty.wav"
-  }
+	{
+		buttonId: 'electro-hat',
+		audioId: 'Q',
+		src: funk1
+	},
+	{
+		buttonId: 'slick-hat',
+		audioId: 'W',
+		src: funk2
+	},
+	{
+		buttonId: 'chirp',
+		audioId: 'E',
+		src: funk3
+	},
+	{
+		buttonId: '80s-snare',
+		audioId: 'A',
+		src: funk4
+	},
+	{
+		buttonId: '80s-tom',
+		audioId: 'S',
+		src: funk5
+	},
+	{
+		buttonId: 'sumo',
+		audioId: 'D',
+		src: funk6
+	},
+	{
+		buttonId: 'tron-kick',
+		audioId: 'Z',
+		src: funk7
+	},
+	{
+		buttonId: 'zapper-kick',
+		audioId: 'X',
+		src: funk8
+	},
+	{
+		buttonId: 'zip',
+		audioId: 'C',
+		src: funk9
+	}
 ];
 
 class DrumMachine extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      selectedBank: "funky",
-      soundBank: FUNKY_BANK,
-      currentSound: ""
-    };
+		this.state = {
+			selectedBank: 'funky',
+			soundBank: FUNKY_BANK,
+			currentSound: ''
+		};
 
-    this.animateButton = this.animateButton.bind(this);
-    this.animateText = this.animateText.bind(this);
-    this.changeTheme = this.changeTheme.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.handleBankChange = this.handleBankChange.bind(this);
-  }
-  componentDidMount() {
-    this.changeTheme();
-  }
-  animateButton(button) {
-    if (!button.classList.contains("pulse")) {
-      button.classList.add("pulse");
-      button.addEventListener("transitionend", function removeAnimation() {
-        button.classList.remove("pulse");
-        button.removeEventListener("transitionend", removeAnimation);
-      });
-    } else {
-      button.classList.remove("pulse");
-    }
-  }
-  animateText() {
-    let displayText = document.getElementById("display");
+		this.animateButton = this.animateButton.bind(this);
+		this.animateText = this.animateText.bind(this);
+		this.changeTheme = this.changeTheme.bind(this);
+		this.handleInput = this.handleInput.bind(this);
+		this.handleBankChange = this.handleBankChange.bind(this);
+	}
+	componentDidMount() {
+		this.changeTheme();
+	}
+	animateButton(button) {
+		if (!button.classList.contains('pulse')) {
+			button.classList.add('pulse');
+			button.addEventListener('transitionend', function removeAnimation() {
+				button.classList.remove('pulse');
+				button.removeEventListener('transitionend', removeAnimation);
+			});
+		} else {
+			button.classList.remove('pulse');
+		}
+	}
+	animateText() {
+		let displayText = document.getElementById('display');
 
-    if (!displayText.classList.contains("fadeText")) {
-      displayText.classList.add("fadeText");
-      displayText.addEventListener("transitionend", function removeAnimation() {
-        displayText.classList.remove("fadeText");
-        displayText.removeEventListener("transitionend", removeAnimation);
-      });
-    } else {
-      displayText.classList.remove("fadeText");
-    }
-  }
-  changeTheme(newBank) {
-    let arr = FUNKY_BANK;
+		if (!displayText.classList.contains('fadeText')) {
+			displayText.classList.add('fadeText');
+			displayText.addEventListener('transitionend', function removeAnimation() {
+				displayText.classList.remove('fadeText');
+				displayText.removeEventListener('transitionend', removeAnimation);
+			});
+		} else {
+			displayText.classList.remove('fadeText');
+		}
+	}
+	changeTheme(newBank) {
+		let arr = FUNKY_BANK;
 
-    if (newBank === "funky") {
-      arr = ACOUSTIC_BANK;
-    }
+		if (newBank === 'funky') {
+			arr = ACOUSTIC_BANK;
+		}
 
-    for (let i = 0; i < arr.length; i++) {
-      let cn = "rainbow" + i;
-      let id = arr[i].buttonId;
-      document.getElementById(id).classList.toggle(cn);
-    }
-  }
-  handleInput(soundObj) {
-    let audio = soundObj.firstChild;
+		for (let i = 0; i < arr.length; i++) {
+			let cn = 'rainbow' + i;
+			let id = arr[i].buttonId;
+			document.getElementById(id).classList.toggle(cn);
+		}
+	}
+	handleInput(soundObj) {
+		let audio = soundObj.firstChild;
 
-    audio.load();
-    audio.play();
+		audio.load();
+		audio.play();
 
-    this.animateButton(soundObj);
-    this.animateText();
-    this.setState({
-      currentSound: soundObj.id
-    });
-  }
-  handleBankChange(bank) {
-    let newBank = ACOUSTIC_BANK;
+		this.animateButton(soundObj);
+		this.animateText();
+		this.setState({
+			currentSound: soundObj.id
+		});
+	}
+	handleBankChange(bank) {
+		let newBank = ACOUSTIC_BANK;
 
-    if (bank === "funky") {
-      newBank = FUNKY_BANK;
-    }
-    this.setState({
-      selectedBank: bank,
-      soundBank: newBank
-    });
-    this.changeTheme(bank);
-  }
-  render() {
-    return (
-      <div className="bg-container">
-        <div id="drum-machine" className="grid">
-          <div className="grid-cell">
-            <h1>Good Beats</h1>
-          </div>
-          <div className="grid-cell">
-            <DisplayBanks
-              onBankChange={this.handleBankChange}
-              selectedBank={this.state.selectedBank}
-            />
-          </div>
-          <div className="grid-cell">
-            <DrumPad
-              soundBank={this.state.soundBank}
-              onInput={this.handleInput}
-            />
-          </div>
-          <div className="grid-cell">
-            <DisplayMessage message={this.state.currentSound} />
-          </div>
-        </div>
-      </div>
-    );
-  }
+		if (bank === 'funky') {
+			newBank = FUNKY_BANK;
+		}
+		this.setState({
+			selectedBank: bank,
+			soundBank: newBank
+		});
+		this.changeTheme(bank);
+	}
+	render() {
+		return (
+			<div className="bg-container">
+				<div id="drum-machine" className="grid">
+					<div className="grid-cell">
+						<h1>Good Beats</h1>
+					</div>
+					<div className="grid-cell">
+						<DisplayBanks onBankChange={this.handleBankChange} selectedBank={this.state.selectedBank} />
+					</div>
+					<div className="grid-cell">
+						<DrumPad soundBank={this.state.soundBank} onInput={this.handleInput} />
+					</div>
+					<div className="grid-cell">
+						<DisplayMessage message={this.state.currentSound} />
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default DrumMachine;
